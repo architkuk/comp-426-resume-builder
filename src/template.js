954dd9b46b90b6a50d7856016fec06dcc0bda6ef
Safal <safalwagle3@gmail.com>
2020-11-24 10:59:51 -0500

    adf

--- src/template.js
+++ src/template.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
+import { Button } from '@material-ui/core'
 import Card from '@material-ui/core/Card';
 import { Navbar, Nav, NavDropdown, FormControl } from 'react-bootstrap';
 import CardActions from '@material-ui/core/CardActions';
