51a0a51ae5db87d220ccb177c6ebf60e5a88c6cd
Archit Kulkarni <architkulkarni@Archits-MacBook-Air.local>
2020-11-24 15:17:55 -0500

    removed unnecessary files

--- src/template.js
+++ src/template.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '@material-ui/core/esm/Card';
-import { Navbar, Nav, NavDropdown, FormControl } from 'react-bootstrap';
 import CardActions from '@material-ui/core/esm/CardActions';
 import CardMedia from '@material-ui/core/esm/CardMedia';
 import CssBaseline from '@material-ui/core/esm/CssBaseline';
@@ -135,45 +134,45 @@ export default function Album() {
 			<main>
 				{/* Hero unit */}
 				<div className={classes.heroContent}>
- 					<Container maxWidth='sm'>
- 						<Typography
- 							component='h1'
- 							variant='h2'
- 							align='center'
- 							color='textPrimary'
- 							gutterBottom
- 						>
- 							Welcome to Resume Builder
- 						</Typography>
- 						<Typography
- 							variant='h5'
- 							align='center'
- 							color='textSecondary'
- 							paragraph
- 						>
- 							Choose a template, create an account, and create beautiful
- 							resumes!
- 						</Typography>
- 						<div className={classes.heroButtons}>
- 							<Grid container spacing={2} justify='center'>
- 								<Grid item>
- 									<Button variant='contained' color='inherit'>
- 										<Link color='inherit' underline='none' href='signup'>
- 											Signup
- 										</Link>
- 									</Button>
- 								</Grid>
- 								<Grid item>
- 									<Button variant='outlined' color='primary'>
- 										<Link color='inherit' underline='none' href='login'>
- 											Login
- 										</Link>
- 									</Button>
- 								</Grid>
- 							</Grid>
- 						</div>
- 					</Container>
- 				</div>
+					<Container maxWidth='sm'>
+						<Typography
+							component='h1'
+							variant='h2'
+							align='center'
+							color='textPrimary'
+							gutterBottom
+						>
+							Welcome to Resume Builder
+						</Typography>
+						<Typography
+							variant='h5'
+							align='center'
+							color='textSecondary'
+							paragraph
+						>
+							Choose a template, create an account, and create beautiful
+							resumes!
+						</Typography>
+						<div className={classes.heroButtons}>
+							<Grid container spacing={2} justify='center'>
+								<Grid item>
+									<Button variant='contained' color='inherit'>
+										<Link color='inherit' underline='none' href='signup'>
+											Signup
+										</Link>
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button variant='outlined' color='primary'>
+										<Link color='inherit' underline='none' href='login'>
+											Login
+										</Link>
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
+				</div>
 				<Container className={classes.cardGrid} maxWidth='md'>
 					{/* End hero unit */}
 					<Grid container spacing={4}>
@@ -204,20 +203,20 @@ export default function Album() {
 			</main>
 			{/* Footer */}
 			<footer className={classes.footer}>
- 				<Typography variant='h6' align='center' gutterBottom>
- 					Footer
- 				</Typography>
- 				<Typography
- 					variant='subtitle1'
- 					align='center'
- 					color='textSecondary'
- 					component='p'
- 				>
- 					Something here to give the footer a purpose!
- 				</Typography>
- 				<Copyright />
- 			</footer>
- 			{/* End footer */}
+				<Typography variant='h6' align='center' gutterBottom>
+					Footer
+				</Typography>
+				<Typography
+					variant='subtitle1'
+					align='center'
+					color='textSecondary'
+					component='p'
+				>
+					Something here to give the footer a purpose!
+				</Typography>
+				<Copyright />
+			</footer>
+			{/* End footer */}
 		</React.Fragment>
 	);
 }
